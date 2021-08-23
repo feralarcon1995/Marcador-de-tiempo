8546d1e2ae6436d058bd859f8eb578a1b8c82783
Fernando Alarcon <feralarcon1995@gmail.com>
2021-08-23 09:33:13 -0300

    arreglo de funcion cronometro, y funcion guardar marcador

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ document.querySelector('#nav').addEventListener('click', e => {
 
 
 //CRONOMETRO
+//ARREGLO CON LAS CLASES
+
 let clases = [
     ['github',
         'breve historia github',
@@ -41,12 +43,19 @@ let iniciarBtn = document.querySelector('.contenedor__btn--iniciar');
 let reiniciarBtn = document.querySelector('.contenedor__btn--reiniciar');
 let marcarBtn = document.querySelector('.contenedor__btn--marcar');
 let temporizador = document.querySelector('.contenedor__temporizador');
-let tiempoContenedor = document.querySelector('.contenedor__resultado');
+let tiempoContenedor = document.querySelector('.contenedor__resultados');
 let seleccionarClase = document.querySelector('select');
+let errorSpan = document.querySelector('.contenedor__error');
 //VARIABLES DE TIEMPO
 let tiempo = 0;
+let hora = 0;
+let minutos = 0;
+let mls = 0;
+let segundos = 0;
+let horaMax, minutosMax, segundosMax;
 let intervalo = 0;
 let verificador = false;
+let posicion = 0;
 
 //LLAMADO DE FUNCION 
 empezarConteo()
@@ -59,8 +68,28 @@ function iniciarContador() {
         iniciarBtn.classList.remove("contenedor__btn--iniciar");
         iniciarBtn.classList.add("contenedor__btn--parar");
         intervalo = setInterval(function() {
-            tiempo += 0.01;
-            temporizador.innerHTML = tiempo.toFixed(2);
+
+            mls++;
+
+            if (mls > 59) {
+                segundos++;
+                mls = 0;
+            }
+            if (segundos > 59) {
+                minutos++;
+                segundos = 0;
+            }
+            if (minutos > 59) {
+                hora++;
+                minutos = 0;
+            }
+
+            if (segundos < 10) { segundosMax = "0" + segundos; } else { segundosMax = segundos; }
+            if (minutos < 10) { minutosMax = "0" + minutos; } else { minutosMax = minutos; }
+            if (hora < 10) { horaMax = "0" + hora; } else { horaMax = hora; }
+
+
+            temporizador.innerHTML = `${horaMax}:${minutosMax}:${segundosMax}`;
         }, 10);
         verificador = true;
     } else { // Y SI ESTA TRUE LO VUELVE FALSO Y FRENA EL INTERVALO DE TIEMPO
@@ -74,9 +103,9 @@ function iniciarContador() {
 
 //FUNCION CON LOS EVENTOS CLICK
 function empezarConteo() {
-    iniciarBtn.addEventListener('click', iniciarContador);
+    iniciarBtn.addEventListener('click', validar);
     reiniciarBtn.addEventListener('click', reiniciarContador);
-    marcarBtn.addEventListener('click', grabarMarcador);
+
 }
 
 //FUNCION RESETEAR CONTADOR 
@@ -86,13 +115,26 @@ function reiniciarContador() {
     iniciarBtn.classList.remove("contenedor__btn--parar");
     iniciarBtn.classList.add("contenedor__btn--iniciar");
     tiempo = 0;
-    temporizador.innerHTML = `${tiempo}.00`;
+    temporizador.innerHTML = `${tiempo}:00`;
     clearInterval(intervalo);
 }
 
+//FUNCION VALIDA QUE SE SELECCIONE UNA CLASE PARA INICIAR
+function validar() {
+    if (seleccionarClase.value === "") {
+        console.log('selecciona')
+        errorSpan.innerHTML = `<p>Selecciona una clase por favor.</p>`
+    } else {
+        errorSpan.innerHTML = "";
+        iniciarContador();
+        marcarBtn.addEventListener('click', grabarMarcador);
+    }
+}
 //FUNCION GUARDAR MARCADOR
 function grabarMarcador() {
     let eleccion = seleccionarClase.value;
-    console.log(clases[eleccion][eleccion]);
+    posicion++;
+    tiempoContenedor.innerHTML +=
+        `<li>${horaMax}:${minutosMax}:${segundosMax} - ${clases[eleccion][posicion]}</li>`;
 
 }
